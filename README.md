44c1a5bab6d4e497e6d4fcd7a9829e5852aa56a4
Artem <admin4@newit-lan.ru>
2019-07-05 18:06:03 +0400

    modified:   README.md 	modified:   mac.py

--- README.md
+++ README.md
@@ -4,10 +4,19 @@ q - выход
 Converting the separator in the MAC address from "-" to ":" and vice versa, checking for the presence of Cyrillic in the address.
 q - quit
 
+
+```
+shell ~ $ ./mac.py 00-17-9A-05-85-56
+00:17:9A:05:85:56
+shell ~ $
+
+```
+
+
 ```
 shell ~ $ git clone --depth=1 https://github.com/anamorfis/mac
 shell ~ $ cd mac
-shell ~ $ ./mac.py 
+shell ~ $ ./mac.py
 mac: 00-17-9A-05-85-56
 00:17:9A:05:85:56
 mac: 00:17:9A:05:85:56
--- mac.py
+++ mac.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 def cyrillic_check(a):
     alphabet = ('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
     find_cyrillic = [x for x in alphabet if x in a.lower()]
@@ -14,10 +16,15 @@ def replace(a):
     if ":" in a:
         print(a.replace(':','-'))
 
-while True:
-    a = str(input("mac: "))
-    if a == "q":
-        break
-    else:
-        if cyrillic_check(a):
-            replace(a)
+if len(sys.argv) > 1 :
+    if cyrillic_check(sys.argv[1]):
+        replace(sys.argv[1])
+else:
+    while True:
+        a = str(input("mac: "))
+        if a == "q":
+            break
+        else:
+            if cyrillic_check(a):
+                replace(a)
+
