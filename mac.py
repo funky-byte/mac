ddf646244c5b18169cd63f2f323eaa37c03e9964
Artem <admin4@newit-lan.ru>
2019-09-12 10:28:34 +0400

    modified:   mac.py

--- mac.py
+++ mac.py
@@ -2,11 +2,13 @@
 
 import sys
 
+
 def help():
     print(
         "Convert mac-address with different separations.\n"
         "Without flags convert from normal mac-address separated by ':' or '-'\n"
         "-c convert from to cisco-like mac-address separated by '.' or '-'\n" )
+
 
 def cyrillic_check(a):
     alphabet = ('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
@@ -16,12 +18,14 @@ def cyrillic_check(a):
         return False
     return True
 
+
 def replace(a):
     if "-" in a:
         print(a.replace('-',':'))
     if ":" in a:
         print(a.replace(':','-'))
- 
+
+
 def cisco_replace(mac):
      if "." in mac:
         mac = mac.replace('.','')
@@ -29,17 +33,22 @@ def cisco_replace(mac):
         print(*out, sep='-')
      if "-" in mac:
         mac = mac.replace('-','')
-        out = [(mac[i:i+4]) for i in range(0, len(mac), 4)]
+        out = [(mac.lower()[i:i+4]) for i in range(0, len(mac), 4)]
         print(*out, sep='.')
-       
+     if ":" in mac:
+        mac = mac.replace(':','')
+        out = [(mac.lower()[i:i+4]) for i in range(0, len(mac), 4)]
+        print(*out, sep='.')
+
+
 def cisco_mac():
     if len(sys.argv) > 2:
         if cyrillic_check(sys.argv[2]):
-            cisco_replace(sys.argv[2])    
+            cisco_replace(sys.argv[2])
     else:
         print("input cisco-like mac or normal mac separated by '-'")
-        return False
- 
+
+
 if len(sys.argv) > 1 :
     if sys.argv[1] == "-h":
         help()
@@ -56,4 +65,3 @@ else:
         else:
             if cyrillic_check(a):
                 replace(a)
-
