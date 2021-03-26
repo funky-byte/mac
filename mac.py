1dc6f00ecda7b3653d477a655fc7829b08dc84be
Artem <admin4@newit-lan.ru>
2021-03-26 10:55:04 +0400

    modified:   mac.py

--- mac.py
+++ mac.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 import sys
+import requests
 
 def help():
     print("\n"
         "Convert mac-address with different separations.\n\n"
-        "Without flags convert from normal mac-address separated by ':' or '-'\n\n"
-        "\033[1m -c \033[0m convert from to cisco-like mac-address separated by '.' or '-'\n" )
+        "\033[1m 1) \033[0m Without flags convert from normal mac-address separated by ':' or '-'\n"
+        "\033[1m 2) -c \033[0m convert from to cisco-like mac-address separated by '.' or '-'\n"
+        "\033[1m 3) -v \033[0m show mac vendor from site macvendors.com \n" )
 
 
 def cyrillic_check(mac):
@@ -47,12 +49,19 @@ def cisco_mac():
     else:
         print("input cisco-like mac or normal mac separated by '-'")
 
+def macvendor():
+    if len(sys.argv)> 2:
+        if cyrillic_check(sys.argv[2]):
+            r = requests.get('https://api.macvendors.com/%s' % sys.argv[2])
+            print("\033[1m %s \033[0m \n" % r.text)
 
 if len(sys.argv) > 1 :
     if sys.argv[1] == "-h":
         help()
     elif sys.argv[1] == "-c":
         cisco_mac()
+    elif sys.argv[1] == "-v":
+        macvendor()
     else:
         if cyrillic_check(sys.argv[1]):
             replace(sys.argv[1])
